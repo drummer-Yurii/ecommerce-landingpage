5c41e482b6a99575f633b797c7eeae9f25f4a6c4
Yurii Rybachok <drummer2009@ukr.net>
2023-02-26 15:51:33 +0200

    add header component & images to project

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,13 @@
 <template>
   <div>
+    <Header />
     <RouterView />
   </div>
 </template>
 
 <script setup>
+import Header from './components/Header.vue';
+
   // import { RouterLink, RouterView } from 'vue-router'
 </script>
 
